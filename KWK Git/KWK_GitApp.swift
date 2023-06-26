074e8bc23402ae12ef12000ad98d0955ed940766
Gemma Brodsky <gembrodsky@gmail.com>
2023-06-26 10:20:39 -0400

    Initial Commit

---
+++ KWK Git/KWK_GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWK_GitApp.swift
+//  KWK Git
+//
+//  Created by nonna vasilchuk on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct KWK_GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
